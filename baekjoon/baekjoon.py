ea8a27a70de0a0ad3b05b3c8570bc247236c88fc
kangsunhyuk <128451536+kangsunhyuk@users.noreply.github.com>
2023-04-10 18:59:58 +0900

    백준

--- baekjoon/baekjoon.py
+++ baekjoon/baekjoon.py
@@ -5,6 +5,7 @@
 # print((a*b)%c)
 # print(((a%c)*(b%c))%c)
 
+
 # 2588번 문제 곱셈
 # a = int(input())
 # b = input()
@@ -16,9 +17,11 @@
 # print(n3)
 # print(a*int(b))
 
+
 # 11382번 문제 A+B+C
 # a, b, c = map(int, input(). split())
 # print(a+b+c)
+
 
 # 10171 문제 고양이
 # print("\\    /\\")
@@ -26,9 +29,43 @@
 # print("(  /  )")
 # print(" \\(__)|")
 
+
 # 10172 문제 개xx
 # print("|\_/|")
 # print("|q p|   /}")
 # print('( 0 )"""\\')
 # print('\\"^"`    |')
 # print("||_/=\\\__ |")
+
+
+# 1330번 문제 두 수 비교하기 
+# a,b = map(int, input().split())
+# if(a>b):
+#     print(">")
+# elif(a<b):
+#     print("<")
+# else:
+#     print("==")
+
+
+# 9498번 문제 시험 성적
+# a=int(input())
+# if (100 >= a >= 90):
+#     print("A")
+# elif(89 >= a >= 80):
+#     print("B")
+# elif(79 >= a >= 70):
+#     print("C")
+# elif(69 >= a >= 60):
+#     print("D")
+# else:
+#     print("F")
+
+
+# 2753번 문제 윤년
+# year = int(input())
+# if ((year%4 == 0)and(year%100 != 0)) or (year%400 == 0):
+#     print(1)
+# else:
+#     print(0)
+
