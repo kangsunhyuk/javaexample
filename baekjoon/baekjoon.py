285661fe913ef562d35b9dba52a93596cdc1789a
kangsunhyuk <128451536+kangsunhyuk@users.noreply.github.com>
2023-04-10 16:51:35 +0900

    백준풀이

---
+++ baekjoon/baekjoon.py
@@ -0,0 +1,34 @@
+# 10430번 문제 나머지
+# a,b,c = map(int, input().split())
+# print((a+b)%c)
+# print(((a%c) + (b%c))%c)
+# print((a*b)%c)
+# print(((a%c)*(b%c))%c)
+
+# 2588번 문제 곱셈
+# a = int(input())
+# b = input()
+# n1 = (a* int(b[2]))
+# n2 = (a* int(b[1]))
+# n3 = (a* int(b[0]))
+# print(n1)
+# print(n2)
+# print(n3)
+# print(a*int(b))
+
+# 11382번 문제 A+B+C
+# a, b, c = map(int, input(). split())
+# print(a+b+c)
+
+# 10171 문제 고양이
+# print("\\    /\\")
+# print(" )  ( ')")
+# print("(  /  )")
+# print(" \\(__)|")
+
+# 10172 문제 개xx
+# print("|\_/|")
+# print("|q p|   /}")
+# print('( 0 )"""\\')
+# print('\\"^"`    |')
+# print("||_/=\\\__ |")
